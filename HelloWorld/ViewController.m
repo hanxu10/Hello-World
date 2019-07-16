d7bd3cb9c3bbdc711b25056c458b48f5e802d143
xuxu <hanxu10@126.com>
2019-07-16 09:04:54 +0800

    add progressHUD

--- HelloWorld/ViewController.m
+++ HelloWorld/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "MBProgressHUD.h"
 
 @interface ViewController ()
 
@@ -16,7 +17,37 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+//    UIImageView *waterImageView = [[UIImageView alloc] initWithFrame:self.view.bounds];
+//    waterImageView.image = [UIImage imageNamed:@"water"];
+//    [self.view addSubview:waterImageView];
+//
+//    UIInterpolatingMotionEffect *motionEffect;
+//    motionEffect = [[UIInterpolatingMotionEffect alloc]initWithKeyPath:@"center.x"
+//                                                                  type:UIInterpolatingMotionEffectTypeTiltAlongHorizontalAxis];
+//    motionEffect.minimumRelativeValue = @(-25);
+//    motionEffect.maximumRelativeValue = @(25);
+//    [waterImageView addMotionEffect:motionEffect];
+//
+//    motionEffect = [[UIInterpolatingMotionEffect alloc]initWithKeyPath:@"center.y"
+//                                                                  type:UIInterpolatingMotionEffectTypeTiltAlongVerticalAxis];
+//    motionEffect.minimumRelativeValue = @(-25);
+//    motionEffect.maximumRelativeValue = @(25);
+//    [waterImageView addMotionEffect:motionEffect];
+    
+    
+    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    hud.defaultMotionEffectsEnabled = YES;
+    hud.mode = MBProgressHUDModeAnnularDeterminate;
+    hud.label.text = @"Loading";
+
+    dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
+        // Do something...
+        sleep(4);
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [MBProgressHUD hideHUDForView:self.view animated:YES];
+        });
+    });
 }
 
 
